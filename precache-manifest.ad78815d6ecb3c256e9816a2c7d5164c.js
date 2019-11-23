1b6447471cf53f3c55c4b63e7f91ea9b079e9a82
saurabh <sm030127@gmail.com>
2019-11-23 17:50:11 +0530

    Updates

---
+++ precache-manifest.ad78815d6ecb3c256e9816a2c7d5164c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d312e951d2e9757e44e6f81e1b1c46c1",
+    "url": "/CountryApp/index.html"
+  },
+  {
+    "revision": "5495735b83f1870cbeff",
+    "url": "/CountryApp/static/css/main.f59e0b24.chunk.css"
+  },
+  {
+    "revision": "563e59981188b5c3e679",
+    "url": "/CountryApp/static/js/2.ce8bf259.chunk.js"
+  },
+  {
+    "revision": "5495735b83f1870cbeff",
+    "url": "/CountryApp/static/js/main.9f5566a7.chunk.js"
+  },
+  {
+    "revision": "b073c810b04a77c82ada",
+    "url": "/CountryApp/static/js/runtime-main.918e4fdf.js"
+  }
+]);
